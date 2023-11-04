4b0057554973d20876e865989b7c1395fee18b20
Thales Spanhol <thales_spanhol@hotmail.com>
2023-11-03 22:01:01 -0300

    Feat: heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Container, Heading } from "@chakra-ui/react"
+import { Container, Heading } from "@chakra-ui/react";
 
 function App() {
   return (
     <>
-    <Heading
-    fontSize={{base: '4xl', sm: '5xl', md: '6xl'}}
-    fontWeight='bold'
-    textAlign='center'
-    bgGradient='linear(to-l, #7928CA, #FF0080)' 
-    bgClip='text'
-    mt={4}
-    >
-      Welcome to DnD Kanban
-    </Heading>
-    <Container maxWidth='container.lg' px={4} py={10}>
-      { /* Colunas aqui */ }
-    </Container>
+      <Heading
+        fontSize={{ base: "4xl", sm: "5xl", md: "6xl" }}
+        fontWeight="bold"
+        textAlign="center"
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        bgClip="text"
+        mt={4}
+      >
+        Welcome to DnD Kanban
+      </Heading>
+      <Container maxWidth="container.lg" px={4} py={10}>
+        {/* Colunas aqui */}
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
