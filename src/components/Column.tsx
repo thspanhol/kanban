4fd74025559b1f76138fa9b04339102950eba2f2
Thales Spanhol <thales_spanhol@hotmail.com>
2023-11-11 23:38:26 -0300

    Feat: column component

---
+++ src/components/Column.tsx
@@ -0,0 +1,18 @@
+import { AddIcon } from "@chakra-ui/icons";
+import {
+    Badge,
+    Box,
+    Heading,
+    IconButton,
+    Stack,
+    useColorModeValue,
+} from "@chakra-ui/react";
+import { ColumnType } from "../utils/enums";
+
+const ColumnColorScheme: = {};
+
+function Column({column}: {column: ColumnType}) {
+
+}
+
+export default Column;
