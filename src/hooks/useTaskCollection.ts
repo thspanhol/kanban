bb36a57b53246f048d00d62fb241c8ac5290c43f
Thales Spanhol <thales_spanhol@hotmail.com>
2023-11-14 22:40:05 -0300

    Feat: use task collection

---
+++ src/hooks/useTaskCollection.ts
@@ -0,0 +1,9 @@
+import { v4 as uuidv4 } from 'uuid'
+import { ColumnType } from '../utils/enums'
+import { TaskModel } from '../utils/models'
+
+function useTaskCollection() {
+
+}
+
+export default useTaskCollection;
