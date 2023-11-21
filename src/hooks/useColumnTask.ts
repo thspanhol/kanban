3690ee37f3e293dffa9a46d004f1d464a204421e
Thales Spanhol <thales_spanhol@hotmail.com>
2023-11-20 21:25:11 -0300

    Feat: use column tasks

--- src/hooks/useColumnTask.ts
+++ src/hooks/useColumnTask.ts
@@ -5,14 +5,11 @@ import { TaskModel } from "../utils/models";
 import useTaskCollection from "./useTaskCollection";
 
 function useColumnTasks(column: ColumnType) {
-    const [tasks, setTasks] = useTaskCollection(); 
+  const [tasks, setTasks] = useTaskCollection();
 
-    const addEmptyTask = useCallback(() => {
-        console.log(`Adding new empty task to ${column} column`);
-        
-
-
-    },[column, setTasks ]);
+  const addEmptyTask = useCallback(() => {
+    console.log(`Adding new empty task to ${column} column`);
+  }, [column, setTasks]);
 }
 
 export default useColumnTasks;
