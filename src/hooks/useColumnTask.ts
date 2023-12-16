07787333f0acf1d5dc2d304e5cdf79de273aefea
Thales Spanhol <thales_spanhol@hotmail.com>
2023-12-15 22:51:00 -0300

    Feat: helpers

--- src/hooks/useColumnTask.ts
+++ src/hooks/useColumnTask.ts
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 import { ColumnType } from "../utils/enums";
 import { TaskModel } from "../utils/models";
 import useTaskCollection from "./useTaskCollection";
+import { pickChakraRandomColor } from "../utils/helpers";
 
 function useColumnTasks(column: ColumnType) {
   const [tasks, setTasks] = useTaskCollection();
