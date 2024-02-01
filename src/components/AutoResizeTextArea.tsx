2f756ef71cd9b53be48a6e4c628cdbae9bd252a8
Thales Spanhol <thales_spanhol@hotmail.com>
2024-01-31 21:20:37 -0300

    Feat: AutoResizeTextArea

--- src/components/AutoResizeTextArea.tsx
+++ src/components/AutoResizeTextArea.tsx
@@ -1,3 +1,7 @@
 import { Textarea, TextareaProps } from "@chakra-ui/react";
 import React from "react";
 import ResizeTextarea from 'react-textarea-autosize';
+
+export const AutoResizeTextarea = React.forwardRef<>(() => {
+
+});
