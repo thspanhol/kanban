73c96843501f18e55b8ac410c20650cacad9751c
Thales Spanhol <thales_spanhol@hotmail.com>
2024-02-15 19:37:12 -0300

    Feat: useTaskDragAndDrop

--- src/hooks/useTaskDragAndDrop.ts
+++ src/hooks/useTaskDragAndDrop.ts
@@ -1,3 +1,13 @@
 import { useDrag } from 'react-dnd';
 import { ItemType } from '../utils/enums';
 import { DragItem, TaskModel } from '../utils/models';
+
+export function useTaskDragAndDrop({
+    task,
+    index,
+}: {
+    task: TaskModel;
+    index: number;
+}) {
+    useDrag<>({})
+}
