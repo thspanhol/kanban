20adca94313e97a5165551450013aa627a03fdf9
Thales Spanhol <thales_spanhol@hotmail.com>
2024-02-25 22:44:52 -0300

    Feat: useColumnDrop

---
+++ src/hooks/useColumnDrop.ts
@@ -0,0 +1,9 @@
+import { useDrop } from 'react-dnd';
+import { ColumnType, ItemType } from '../utils/enums';
+import { DragItem, TaskModel } from '../utils/models';
+
+function useColumnDrop(
+
+) {}
+
+export default useColumnDrop;
