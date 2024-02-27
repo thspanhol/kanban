8ab94fae1da01807240c4a4f18d406a08bc805a1
Thales Spanhol <thales_spanhol@hotmail.com>
2024-02-26 22:45:40 -0300

    Feat: useColumnDrop

--- src/hooks/useColumnDrop.ts
+++ src/hooks/useColumnDrop.ts
@@ -3,7 +3,8 @@ import { ColumnType, ItemType } from '../utils/enums';
 import { DragItem, TaskModel } from '../utils/models';
 
 function useColumnDrop(
-
+    column: ColumnType,
+    handleDrop: (fromColumn: ColumnType, taskId: TaskModel['id']) => void,
 ) {}
 
 export default useColumnDrop;
