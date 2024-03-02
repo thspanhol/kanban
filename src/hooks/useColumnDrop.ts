276a03225c7c3382ea5258964c41d139ae7fcb6c
Thales Spanhol <thales_spanhol@hotmail.com>
2024-03-01 23:07:18 -0300

    Feat: useColumnDrop

--- src/hooks/useColumnDrop.ts
+++ src/hooks/useColumnDrop.ts
@@ -1,21 +1,21 @@
-import { useDrop } from 'react-dnd';
-import { ColumnType, ItemType } from '../utils/enums';
-import { DragItem, TaskModel } from '../utils/models';
+import { useDrop } from "react-dnd";
+import { ColumnType, ItemType } from "../utils/enums";
+import { DragItem, TaskModel } from "../utils/models";
 
 function useColumnDrop(
-    column: ColumnType,
-    handleDrop: (fromColumn: ColumnType, taskId: TaskModel['id']) => void,
+  column: ColumnType,
+  handleDrop: (fromColumn: ColumnType, taskId: TaskModel["id"]) => void
 ) {
-    const [{ isOver }, dropRef] = useDrop<DragItem, void, { isOver: boolean }>({
-        accept: ItemType.TASK,
-        drop: (dragItem) => {
-            if (!dragItem || dragItem.from === column) {
-                return;
-            }
+  const [{ isOver }, dropRef] = useDrop<DragItem, void, { isOver: boolean }>({
+    accept: ItemType.TASK,
+    drop: (dragItem) => {
+      if (!dragItem || dragItem.from === column) {
+        return;
+      }
 
-            handleDrop(dragItem.from, dragItem.id);
-        },
-    });
+      handleDrop(dragItem.from, dragItem.id);
+    },
+  });
 }
 
 export default useColumnDrop;
