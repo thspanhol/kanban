5c6eab6d515000c482000329b2801b53607cbcd6
Thales Spanhol <thales_spanhol@hotmail.com>
2024-04-09 23:56:11 -0300

    Feat: Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Container, Heading, SimpleGrid } from "@chakra-ui/react";
 import Column from "./components/Column";
 import { ColumnType } from "./utils/enums";
-import { DndProvider } from 'react-dnd';
-import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
 
 function App() {
   return (
@@ -18,13 +18,16 @@ function App() {
         Welcome to DnD Kanban
       </Heading>
       <Container maxWidth="container.lg" px={4} py={10}>
-      <DndProvider backend={HTML5Backend}>
-        <SimpleGrid columns={{ base: 1, md: 4 }} spacing={{ base: 16, md: 4 }}>
-          <Column column={ColumnType.TO_DO} />
-          <Column column={ColumnType.IN_PROGRESS} />
-          <Column column={ColumnType.BLOCKED} />
-          <Column column={ColumnType.COMPLETED} />
-        </SimpleGrid>
+        <DndProvider backend={HTML5Backend}>
+          <SimpleGrid
+            columns={{ base: 1, md: 4 }}
+            spacing={{ base: 16, md: 4 }}
+          >
+            <Column column={ColumnType.TO_DO} />
+            <Column column={ColumnType.IN_PROGRESS} />
+            <Column column={ColumnType.BLOCKED} />
+            <Column column={ColumnType.COMPLETED} />
+          </SimpleGrid>
         </DndProvider>
       </Container>
     </>
