3c7ca3ca5731146a78229c24ac827454761dab35
Thales Spanhol <thales_spanhol@hotmail.com>
2024-04-11 23:31:58 -0300

    Feat: DarkModeIconButton

---
+++ src/components/DarkModeIconButton.tsx
@@ -0,0 +1,7 @@
+import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { IconButton, useColorMode } from '@chakra-ui/react';
+import React from 'react';
+
+function DarkModeIconButton() {
+    
+}
