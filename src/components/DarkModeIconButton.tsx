dc291aa946cae8c47c48a944d34b42e866da78cb
Thales Spanhol <thales_spanhol@hotmail.com>
2024-04-23 23:16:19 -0300

    Feat: DarkModeIconButton

--- src/components/DarkModeIconButton.tsx
+++ src/components/DarkModeIconButton.tsx
@@ -1,23 +1,22 @@
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
-import { IconButton, useColorMode } from '@chakra-ui/react';
-import React from 'react';
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { IconButton, useColorMode } from "@chakra-ui/react";
+import React from "react";
 
 function DarkModeIconButton({
-    ...rest
+  ...rest
 }: React.ComponentPropsWithoutRef<typeof IconButton>) {
-    const { colorMode, toggleColorMode } = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
 
-    const isDark = colorMode === 'dark';
+  const isDark = colorMode === "dark";
 
-    return (
-        <IconButton
-        onClick={toggleColorMode}
-        icon={isDark ? <MoonIcon /> : <SunIcon />}
-        aria-label={'dark-mode-toggle'}
-        {...rest}
-        />
-    );
-    
+  return (
+    <IconButton
+      onClick={toggleColorMode}
+      icon={isDark ? <MoonIcon /> : <SunIcon />}
+      aria-label={"dark-mode-toggle"}
+      {...rest}
+    />
+  );
 }
 
 export default DarkModeIconButton;
